5f41928c49172dc99fdd60359c1dd4b4e44e9f03
iemong <kiyoshidainagon@gmail.com>
2019-12-29 17:49:12 +0900

    tab仕込み忘れ

--- reducers/index.ts
+++ reducers/index.ts
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux'
 import user, { State as UserState } from 'reducers/user'
+import tab, { State as TabState } from 'reducers/tab'
 
 export interface State {
     rootReducer: {
         user: UserState
+        tab: TabState
     }
 }
 
 export default combineReducers({
     user,
+    tab,
 })
